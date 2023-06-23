d1ead433a5553601e29ff45bf7c5b727614fc63d
Philip <seongju.lee90@gmail.com>
2023-06-23 05:10:50 -0700

    added optional chaining, changed my profile page css

--- react-app/src/components/SplashReviewCard/index.js
+++ react-app/src/components/SplashReviewCard/index.js
@@ -13,14 +13,14 @@ function SplashReviewCard({reviews}) {
     // if review has no img, put broken img icon otherwise, choose one from the array
     let reviewImg;
 
-    if (!reviewImageArr.length) {
+    if (!reviewImageArr?.length) {
         reviewImg = brokenImg
     } else {
         // math.random * array length scale the random decimal number to the length of the array
         // inclusive of 0 and exclusive the length of array, this ensures will fall in valid range of array
         // math.floor to round it to a whole number
-        const randomIndex = Math.floor(Math.random() * reviewImageArr.length)
-        reviewImg = reviewImageArr[randomIndex].url
+        const randomIndex = Math.floor(Math.random() * reviewImageArr?.length)
+        reviewImg = reviewImageArr[randomIndex]?.url
     }
 
     const navigateToSingleRestaurant = () => {
