c5dbb623f4b8976d15f81d2f04e8f1298a6e896a
Philip <seongju.lee90@gmail.com>
2024-02-01 15:53:55 -0800

    google maps still not working

--- react-app/src/components/RestaurantsIndex/index.js
+++ react-app/src/components/RestaurantsIndex/index.js
@@ -3,7 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { thunkAllRestaurants } from '../../store/restaurants';
 import RestaurantCard from '../RestaurantCard';
+import Map from '../GoogleMap';
 import './RestaurantIndex.css';
+
 
 import map from '../../icons/google-map.png'
 import { thunkSearchRestaurants } from '../../store/search';
@@ -57,6 +59,7 @@ export default function RestaurantsIndex() {
             </div>
             <div className="google-map-container">
                 <img className="index-map-img" src={map} alt={"restaurant-index-map"}/>
+                <Map/>
             </div>
         </div>
     )
